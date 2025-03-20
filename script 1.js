c502247fecf82a44fb3b1c8a19a545f45eb67562
mordecai <heitor_emmerich@estudante.sesisenai.org.br>
2025-03-20 09:21:39 -0300

    ADD - exercicio 2 e mudanças

--- script 1.js
+++
@@ -1,8 +0,0 @@
-let verificarpar = prompt("digite um numero")
-
-    if (0 == verificarpar % 2){
-        console.log("é par");
-    } else {
-        console.log("é impar")
-    }
-    
---
+++ script.js
@@ -0,0 +1,32 @@
+//atividade 1
+let number = prompt("digite um numero")
+
+    function verificarpar(number){
+    if (0 == number % 2){
+        console.log("é par");
+    } else {
+        console.log("é impar")
+    }
+    }
+verificarpar(number)
+
+//Atividade 2
+let Pnumero = parseInt(prompt("Me fale um numero"));
+let Snumero = parseInt(prompt("Me fale outro numero"));
+
+let soma = (Pnumero+Snumero);
+let subtracao = (Pnumero-Snumero);
+let multi = (Pnumero*Snumero);
+
+function calculos(){
+    console.log("a Soma deles é: " +soma);
+    console.log("a Subtração deles é: " +subtracao);
+    console.log("a Multiplicação deles é: " +multi);
+    if (Snumero == 0){
+        console.log("ta maluco doidão divisão por zero!")
+     } else {
+     let divisao = (Pnumero/Snumero) 
+    console.log("a Divisão deles é: " +divisao)}
+}
+
+calculos()
