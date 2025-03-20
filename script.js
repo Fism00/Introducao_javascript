286cd437fb12f36bd06de35aafc389a98340c1b4
mordecai <heitor_emmerich@estudante.sesisenai.org.br>
2025-03-20 09:43:30 -0300

    ADD - Correção

--- script.js
+++ script.js
@@ -1,34 +1,43 @@
 //atividade 1
 function atividade1(){
-let number = prompt("digite um numero");
+let number = prompt('digite um numero');
 
     function verificarpar(number){
     if (0 == number % 2){
-        console.log("é par");
+        console.log('é par');
     } else {
-        console.log("é impar");
+        console.log('é impar');
     }
 }
  verificarpar(number);
 }
 //Atividade 2
 function atividade2(){
-let Pnumero = parseInt(prompt("Me fale um numero"));
-let Snumero = parseInt(prompt("Me fale outro numero"));
+let Pnumero = parseFloat(prompt('Me fale um numero'));
+let Snumero = parseFloat(prompt('Me fale outro numero'));
 
-let soma = (Pnumero+Snumero);
-let subtracao = (Pnumero-Snumero);
-let multi = (Pnumero*Snumero);
+function calculos(num1,num2){
+const soma = num1+num2;
+const subtracao = num1-num2;
+const multi = num1*num2;
 
-function calculos(){
-    console.log("a Soma deles é: " +soma);
-    console.log("a Subtração deles é: " +subtracao);
-    console.log("a Multiplicação deles é: " +multi);
-    if (Snumero == 0){
-        console.log("ta maluco doidão divisão por zero!")
+
+    console.log('a Soma deles é: ' +soma);
+    console.log(' Subtração deles é: ' +subtracao);
+    console.log('a Multiplicação deles é: ' +multi);
+    if (num2 == 0){
+        console.log('ta maluco doidão divisão por zero!')
      } else {
-     let divisao = (Pnumero/Snumero) 
-    console.log("a Divisão deles é: " +divisao)}
+     const divisao = num1/num2 
+    console.log('a Divisão deles é: ' +divisao)}
 }
-calculos()
+calculos(Pnumero,Snumero)
+}
+
+//Atividade 3
+function atividade3(){
+    for (let i = 10; i >= 1; i++ ){
+        console
+    }
+
 }
